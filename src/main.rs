4aa21ae545a4fbc027a388cab7179523f31b551d
Lukasz Orlikowski <lukas9o@wp.pl>
2022-03-23 00:57:09 +0100

    solved task

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,11 @@ enum MessageType {
 
 impl MessageType {
     fn header(&self) -> &'static str {
-        todo!()
+        match self {
+            MessageType::Handshake => "[HANDSHAKE]",
+            MessageType::Post => "[POST]",
+            MessageType::GetCount => "[GET COUNT]",
+        }
     }
 }
 
@@ -69,7 +73,18 @@ impl Client {
     // Method should return an error when a connection already exists.
     // The client should send a handshake to the server.
     fn open(&mut self, addr: &str, server: Server) -> CommsResult<()> {
-        todo!()
+        let msg = Message {
+            msg_type: MessageType::Handshake,
+            load: self.ip.clone(),
+        };
+        if matches!(self.connections.get(addr), Some(_)) {
+            Err(CommsError::ConnectionExists(addr.to_string()))
+        } else {
+            self.connections
+                .insert(addr.to_string(), Connection::Open(server));
+            self.send(addr, msg)?;
+            Ok(())
+        }
     }
 
     // Sends the provided message to the server at the given `addr`.
@@ -77,21 +92,36 @@ impl Client {
     // responds with a ServerLimitReached error, its corresponding connection
     // should be closed.
     fn send(&mut self, addr: &str, msg: Message) -> CommsResult<Response> {
-        // server.receive(msg)
-        todo!()
+        let connection = self.connections.get_mut(addr);
+        match connection {
+            None => Err(CommsError::ConnectionNotFound(addr.to_string())),
+            Some(Connection::Closed) => Err(CommsError::ConnectionClosed(addr.to_string())),
+            Some(Connection::Open(server)) => {
+                let response = server.receive(msg);
+                if matches!(response, Err(CommsError::ServerLimitReached(_))) {
+                    self.connections
+                        .insert(addr.to_string(), Connection::Closed);
+                }
+                response
+            }
+        }
     }
 
     // Returns whether the connection to `addr` exists and has
     // the `Open` status.
     #[allow(dead_code)]
     fn is_open(&self, addr: &str) -> bool {
-        todo!()
+        let connection = self.connections.get(addr);
+        matches!(connection, Some(Connection::Open(_)))
     }
 
     // Returns the number of closed connections
     #[allow(dead_code)]
     fn count_closed(&self) -> usize {
-        todo!()
+        self.connections
+            .iter()
+            .filter(|(_, connection)| matches!(connection, Connection::Closed))
+            .count()
     }
 }
 
@@ -101,7 +131,6 @@ enum Response {
     PostReceived,
     GetCount(u32),
 }
-
 
 #[derive(Clone)]
 struct Server {
@@ -113,7 +142,12 @@ struct Server {
 
 impl Server {
     fn new(name: String, limit: u32) -> Server {
-        todo!()
+        Server {
+            name,
+            post_count: 0,
+            limit,
+            connected_client: None,
+        }
     }
 
     // Consumes the message.
@@ -123,8 +157,24 @@ impl Server {
     // with the GetCount response containing the number of received POST requests.
     fn receive(&mut self, msg: Message) -> CommsResult<Response> {
         eprintln!("{} received:\n{}", self.name, msg.content());
-
-        todo!()
+        match msg.msg_type {
+            MessageType::GetCount => Ok(Response::GetCount(self.post_count)),
+            MessageType::Handshake => match self.connected_client {
+                None => {
+                    self.connected_client = Some(msg.load);
+                    Ok(Response::HandshakeReceived)
+                }
+                Some(_) => Err(CommsError::UnexpectedHandshake(self.name.clone())),
+            },
+            MessageType::Post => {
+                if self.post_count == self.limit {
+                    Err(CommsError::ServerLimitReached(self.name.clone()))
+                } else {
+                    self.post_count += 1;
+                    Ok(Response::PostReceived)
+                }
+            }
+        }
     }
 }
 
